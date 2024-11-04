60de1c262ab4405fc5fba11677745ead1c091b6b
Hann <ejongwon7@gmail.com>
2024-11-04 23:49:22 +0900

    bellman ford with cupy

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bellman-Ford Algorithm with Cupy
+Making python code of Bellman-Ford algorithm using Cupy.
+
+Very simple version, using distance matrix.
+
+## Requirements
+1. `CUDA`
+2. `Cupy`
+3. `Anaconda(Optional)`
+
+### Environments
+- `CUDA Driver Version: 560.94`
+- `CUDA Version: 12.4`
+- `Cupy Version: 13.3.0`
+- `Anaconda Version: 24.9.1`
---
+++ bellman_ford.py
@@ -0,0 +1,24 @@
+import cupy as cp
+
+Inf = 2 ** 30 - 1
+
+
+def bellman_ford(edges, n_vertices, start):
+    distance = cp.asarray([Inf] * n_vertices, dtype=cp.int32)
+    distance[start] = 0
+
+    for _ in range(n_vertices):
+        distance = cp.min(distance + edges, axis=-1)       
+    
+    return distance
+
+edges = [
+    [0, Inf, Inf, Inf],
+    [5, 0, 2, Inf],
+    [3, Inf, 0, Inf],
+    [Inf, 6, 4, 0]
+]
+
+edges = cp.asarray(edges, dtype=cp.int32)
+
+print(bellman_ford(edges, 4, 0))
